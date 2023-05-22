6b3bfd2cb9db858ba81256d718164d329e98c1f8
ichisei <132669382+ichisei@users.noreply.github.com>
2023-05-22 15:15:34 +0000

    [Start]kadai3

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+end
