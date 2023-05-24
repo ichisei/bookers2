bd97047a658ddf8959db50037d9877ed97f0a735
ichisei <132669382+ichisei@users.noreply.github.com>
2023-05-24 14:46:54 +0000

    [ADD]kadai3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,16 +6,21 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    render :books
+    end
   end
 
   def index
     @books = Book.all
+    @book = @books.current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def edit
@@ -35,14 +40,10 @@ class BooksController < ApplicationController
     redirect_to books_path(book.id)
   end
 
-
-
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
-
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
 
   belongs_to :user
 
+  validates :title, presence: true  #, length: { maximum: 200 }
+  validates :body, presence: true
+
 end
