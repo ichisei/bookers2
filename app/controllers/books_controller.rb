620aaeb85f49934f7cc724486e51ae18bc1a1cfb
ichisei <132669382+ichisei@users.noreply.github.com>
2023-05-25 15:56:53 +0000

    [ADD]kadai3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
     else
     render :books
@@ -15,7 +16,8 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = @books.current_user
+    @book = Book.new
+    @user = current_user
   end
 
   def show
@@ -30,6 +32,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(book.id)
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,15 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = current_user
+    @book = Book.new
+
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
+
   end
 
   def edit
@@ -15,12 +19,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
+    redirect_to user_path(@user.id)
   end
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name,:introduction, :profile_image)
   end
 end
