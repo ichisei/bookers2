72fbe8fd58bbe5cb851f16ea8e38bf69d5248325
ichisei <132669382+ichisei@users.noreply.github.com>
2023-05-27 08:20:56 +0000

    [ADD]kadai3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-    flash[:notice] = "You have created book successfully."
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
+      @user = current_user
+      @books = Book.all
       render :index
     end
   end
@@ -30,10 +32,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
 
